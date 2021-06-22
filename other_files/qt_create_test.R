7528dfff210de46897110cb133f3edd1faf06f23
Derek Friend <dfriend21@gmail.com>
2021-06-22 15:13:12 -0700

    modified test files

--- other_files/qt_create_test.R
+++ other_files/qt_create_test.R
@@ -15,6 +15,20 @@ l_iv
 qt = qt_create(1-l_iv, .8, adj_type="expand")
 qt = qt_create(1-l_iv, .2, adj_type="expand")
 #qt = qt_create(1-l_iv, .8, max_cell_length = 10000, adj_type="resample", resample_n_side = 256)
-qt_plot(qt, crop=TRUE)
+qt_plot(qt, crop=TRUE, nb=TRUE)
 qt$maxCellDims()
 #qt_plot(qt, crop=TRUE,border_col="transparent")
+
+qext = qt_extent_orig(qt)
+pts = cbind(runif(100,qext[1], qext[2]), runif(100,qext[3],qext[4]))
+qt_extract(qt, pts)
+qt_plot(qt,crop=TRUE, border_col="gray60")
+points(pts)
+
+pt1 = c(644571,3910000)
+pt2 = c(650000, 3940000)
+qt_plot(qt, border_col="gray50",crop=TRUE)
+points(rbind(pt1,pt2))
+spf = qt_lcp_finder(qt, pt1)
+lcp = qt_find_lcp(spf, pt2)
+lcp
--- other_files/read_write_test.R
+++ other_files/read_write_test.R
@@ -5,11 +5,16 @@ l = raster("/Users/dfriend/Documents/clark_county_project/data/CC Habitat model 
 #==================
 #TEST 1
 path = "/Users/dfriend/Documents/r_packages/quadtree/other_files/output/habQT_test.qtree"
-qt = qt_create(1-l, 0, adj_type="expand")
+qt = qt_create(1-l, .3, adj_type="expand")
 qt_write(qt, path)
 qt2 = qt_read(path)
 qt_plot(qt2, border_col = "transparent")
 
-spf = qt_lcp_finder(qt, c(600000,4000000))
-lcp = qt_find_lcp(spf, c(750000,4100000))
+pt1 = c(600000,4000000)
+pt2 = c(750000,4100000)
+spf = qt_lcp_finder(qt, pt1)
+lcp = qt_find_lcp(spf, pt2)
+points(rbind(pt1,pt2))
+qt_extract(qt, rbind(pt1,pt2))
 lines(lcp)
+lcp
