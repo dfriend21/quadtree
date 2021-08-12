b1962bb7159171e8bbf08ece9ca1c5512846f84a
Derek Friend <dfriend21@gmail.com>
2021-08-12 16:06:21 -0700

    switched data documentation to using Roxygen

---
+++ R/data_documentation.R
@@ -0,0 +1,36 @@
+#' @name habitat
+#' @aliases habitat_roads
+#' @docType data
+#' @title Sample raster data for the 'quadtree' package
+#' @description
+#' \code{habitat} is a raster containing habitat suitability values where each cell
+#' takes on a value between 0 and 1. 
+#' 
+#' \code{habitat_roads} has the exact same footprint as 'habitat', but the values
+#' represent the presence/absence of roads in that cell. 1 indicates presence,
+#' while 0 indicates absence.
+#' @usage 
+#' data("habitat")
+#' data("habitat_roads")
+#' @format \code{RasterLayer} (from the 'raster' package)
+#' @details
+#' These rasters are included for two reasons. First, they provide the datasets
+#' that are used for the code examples in the help files. Second, they provide
+#' easy-to-access datasets for users to experiment with when learning how to use
+#' the 'quadtree' package.
+#' @examples
+#' data("habitat")
+#' data("habitat_roads")
+#' 
+#' par(mfrow=c(1,2))
+#' plot(habitat)
+#' plot(habitat_roads)
+#' 
+#' #use them to make quadtrees
+#' qt1 = qt_create(habitat, .1)
+#' qt2 = qt_create(habitat_roads, .1)
+#' 
+#' par(mfrow=c(1,2))
+#' qt_plot(qt1,crop=TRUE,na_col=NULL,border_lwd=.3)
+#' qt_plot(qt2,crop=TRUE,na_col=NULL,border_lwd=.3)
+NULL
