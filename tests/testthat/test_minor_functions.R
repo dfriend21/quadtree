76cbd5e263e87f6c7421009254ea59ad26daaf90
Derek Friend <dfriend21@gmail.com>
2021-08-13 16:09:41 -0700

    added more unit tests

---
+++ tests/testthat/test_minor_functions.R
@@ -0,0 +1,79 @@
+test_that("qt_as_data_frame runs without errors and produces expected output",{
+  data(habitat)
+  qt = expect_error(qt_create(habitat,.4),NA)
+  df = qt_as_data_frame(qt)
+  expect_s3_class(df, "data.frame")
+  expect_true(nrow(df) > 0)
+  expect_true(qt$nNodes() == nrow(df))
+})
+
+test_that("qt_copy runs without errors and produces expected output",{
+  data(habitat)
+  qt1 = qt_create(habitat,.3,split_method="sd")
+  qt2 = expect_error(qt_copy(qt1),NA)
+  expect_s4_class(qt2,"Rcpp_quadtree")
+  df1 = qt_as_data_frame(qt1)
+  df2 = qt_as_data_frame(qt2)
+  #first test w/o the value column because NA columns mess up the equality check
+  expect_true(all(df1[,-1*which(names(df1)=="value")] == df2[,-1*which(names(df2)=="value")]))
+  #now do the 'value' column separately
+  expect_true(all(df1$value == df2$value,na.rm=TRUE))
+  expect_true(all(which(is.na(df1$value)) == which(is.na(df2$value))))
+})
+
+test_that("qt_extent runs without errors and produces expected output",{
+  library(raster)
+  data(habitat)
+  qt1 = qt_create(habitat,.3,split_method="sd")
+  expect_error(qt_extent(qt1,original=FALSE),NA)
+  qt_ext = expect_error(qt_extent(qt1,original=TRUE),NA)
+  expect_true(qt_ext == extent(habitat))
+})
+
+test_that("qt_extract runs without errors and returns correct values",{
+  library(raster)
+  data(habitat)
+  qt1 = qt_create(habitat,0) #use 0 to make sure everything gets split as small as possible
+  
+  ext = extent(habitat)
+  pts = cbind(runif(1000, ext[1], ext[2]), runif(1000, ext[3], ext[4]))  
+  rst_ext = extract(habitat,pts)
+  qt_ext = expect_error(qt_extract(qt1,pts),NA)
+  rst_ext[is.na(rst_ext)] = -1
+  qt_ext[is.na(qt_ext)] = -1
+  expect_true(all(qt_ext == rst_ext))
+
+  #make sure it works with the 'extents=TRUE' option too  
+  qt_ext2 = expect_error(qt_extract(qt1,pts,extents=TRUE),NA)
+  expect_true("matrix" %in% class(qt_ext2))
+  expect_true(nrow(qt_ext2) > 0)
+  nums = qt_ext2[,"value"]
+  nums[is.na(nums)] = -1
+  expect_true(all(nums == rst_ext))
+})
+
+test_that("qt_proj4string runs without errors and returns correct value",{
+  data(habitat)
+  suppressWarnings({crs(habitat) = "+init=EPSG:27700"})
+  qt1 = qt_create(habitat,.5)
+  qt_proj = expect_error(qt_proj4string(qt1),NA)
+  expect_true(qt_proj == proj4string(habitat))
+})
+
+test_that("reading and writing quadtrees works",{
+  data(habitat)
+  qt1 = qt_create(habitat,.1)
+  filepath = tempfile()
+  expect_error(qt_write(qt1,filepath),NA)
+  qt2 = expect_error(qt_read(filepath),NA)
+  
+  df1 = qt_as_data_frame(qt1)
+  df2 = qt_as_data_frame(qt2)
+  #first test w/o the value column because NA columns mess up the equality check
+  expect_true(all(df1[,-1*which(names(df1)=="value")] == df2[,-1*which(names(df2)=="value")]))
+  #now do the 'value' column separately
+  expect_true(all(df1$value == df2$value,na.rm=TRUE))
+  expect_true(all(which(is.na(df1$value)) == which(is.na(df2$value))))
+  
+  unlink(filepath)
+})
--- tests/testthat/test_qt_create.R
+++ tests/testthat/test_qt_create.R
@@ -1,18 +1,109 @@
-# test_that("examples run",{
+test_that("we can create a quadtree from a matrix",{
+  mat = matrix(runif(16),4)
+  qt = qt_create(mat,split_threshold=.2,split_method="range",combine_method="mean")
+  expect_s4_class(qt,"Rcpp_quadtree")
+})
+
+test_that("we can create a quadtree from a raster",{
+  mat = matrix(runif(16),4)
+  rast = raster::raster(mat)
+  qt = qt_create(rast,split_threshold=.2, split_method="range",combine_method="mean")
+  expect_s4_class(qt,"Rcpp_quadtree")
+})
+
+test_that("quadtree creation with templates works",{
+  data(habitat)
+  data(habitat_roads)
+  qt1 = qt_create(habitat_roads, .1)
+  qt2 = qt_create(habitat, template_quadtree = qt1)
+  qt1_df = qt_as_data_frame(qt1)
+  qt2_df = qt_as_data_frame(qt2)
+  expect_true(all(dim(qt1_df) == dim(qt2_df)))
+  qt1_df2 = qt1_df[,-1*which(names(qt1_df) == "value")]
+  qt2_df2 = qt2_df[,-1*which(names(qt2_df) == "value")]
+  expect_true(all(qt1_df2 == qt2_df2))
+})
+
+test_that("qt_create runs without errors for all parameter settings",{
+  library(raster)
+  
+  # retrieve the sample data
+  data(habitat)
+  rast = habitat
+  expect_error(qt_create(rast, .15),NA)
+  expect_error(qt_create(rast, .15, adj_type="resample", resample_n_side = 128),NA)
+  expect_error(qt_create(rast, .15, adj_type="resample", resample_n_side = 128, resample_pad_NAs=FALSE),NA)
+  expect_error(qt_create(rast, .15, max_cell_length = 1000),NA)
+  expect_error(qt_create(rast, .15, min_cell_length = 1000),NA)
+  
+  qt = qt_create(rast, .15, min_cell_length = 1000)
+  expect_true(qt$root()$smallestChildSideLength() == 1000)
+  
+  expect_error(qt_create(rast, .15, split_if_all_NA=TRUE),NA)
+  expect_error(qt_create(rast, .15, split_if_any_NA=FALSE),NA)
+  expect_error(qt_create(rast, .15, split_if_any_NA=FALSE, max_cell_length=1000),NA)
+  expect_error(qt_create(rast, .15, split_method = "range"),NA)
+  expect_error(qt_create(rast, .15, split_method = "sd"),NA)
+  expect_error(qt_create(rast, .15, combine_method = "mean"),NA)
+  expect_error(qt_create(rast, .15, combine_method = "median"),NA)
+  expect_error(qt_create(rast, .15, combine_method = "min"),NA)
+  expect_error(qt_create(rast, .15, combine_method = "max"),NA)
+  expect_error(qt_create(rast, .15, split_method = "sd", combine_method="min"),NA)
+  #----
+  split_fun = function(vals, args){ 
+    if(any(is.na(vals))){ #check for NAs first
+      return(TRUE); #if there are any NAs we'll split automatically
+    } else {
+      return(any(vals < args$threshold))
+    }
+  }
+  expect_error(qt_create(rast, split_method="custom", split_fun=split_fun, split_args=list(threshold=.8)),NA)
+  #----
+  cmb_fun = function(vals, args){
+    if(any(is.na(vals))){
+      return(NA)
+    }
+    if(mean(vals) < args$threshold){
+      return(args$low_val)
+    } else {
+      return(args$high_val)   
+    } 
+  }
+  expect_error(qt_create(rast, .1, combine_method="custom", combine_fun = cmb_fun, combine_args = list(threshold=.5, low_val=0, high_val=1)),NA)
+  #----
+  cmb_fun2 = function(vals, args){
+    return(max(vals) - min(vals))
+  }
+  expect_error(qt_create(rast, .1, combine_method="custom", combine_fun = cmb_fun2),NA)
+  #----
+  data(habitat_roads)
+  template = habitat_roads
+  split_if_one = function(vals, args){
+    if(any(vals == 1, na.rm=TRUE)) return(TRUE)
+    return(FALSE)
+  }
+  qt_template = qt_create(template, .1)
+  expect_error(qt_create(rast, template_quadtree = qt_template),NA)
+})
+
+# test_that("reading and writing quadtrees works",{
+#   data(habitat)
+#   qt1 = qt_create(habitat,.1)
+#   filepath = "/Users/dfriend/Desktop/testing/testingqt1.qt"
+#   qt_write(qt1,filepath)
+#   qt2 = qt_read(filepath)
+#   qt_plot(qt1)
+#   qt_plot(qt2)
+# })
+# # 
+# test_that("edge cases of qt_create respond appropriately",{
+#   expect_error(qt_create(NULL,1))
+#   qt_create(matrix(),1)
+# })
+#test_that("examples run",{
 #   devtools::run_examples("quadtree")
 # })
-# # test_that("we can create a quadtree from a matrix"){
-# #   mat = matrix(runif(16),4)
-# #   qt = qt_create(mat,split_threshold=.2,split_method="range",combine_method="mean")
-# #   expect_s4_class(qt,"Rcpp_quadtree")
-# # }
-# # 
-# # test_that("we can create a quadtree from a raster"){
-# #   mat = matrix(runif(16),4)
-# #   rast = raster::raster(mat)
-# #   qt = qt_create(rast,split_threshold=.2, split_method="range",combine_method="mean")
-# #   expect_s4_class(qt,"Rcpp_quadtree")
-# # }
+
 # # 
 # # 
 # # 
