bad620c6397f2c24abf8d7e85243d3c802f91fba
Derek Friend <dfriend21@gmail.com>
2021-08-18 09:41:53 -0700

    added 'qt_get_neighbors()'

---
+++ R/qt_get_neighbors.R
@@ -0,0 +1,36 @@
+#' @title Get the Neighbors of a Quadtree Cell
+#' @description Returns a matrix with information about the neighbors of a 
+#' quadtree cell
+#' @param quadtree quadtree object
+#' @param pt numeric vector of length 2; the x and y coordinates of a point - 
+#' this is used to identify which quadtree cell to find neighbors for.
+#' @return 
+#' A 6-column matrix with one row per neighboring cell. It has the following
+#' columns: \itemize{
+#'     \item \code{id}: the ID of the cell
+#'     \item \code{xmin}, \code{xmax}, \code{ymin}, \code{ymax}: the x and y
+#'     limits of the cell
+#'     \item \code{value}: the value of the cell
+#'   }
+#'   
+#' Note that this return matrix only includes terminal nodes/cells - that is,
+#' cells that have no children. 
+#' @examples
+#' data(habitat)
+#' rast = habitat
+#' 
+#' # create a quadtree
+#' qt = qt_create(rast, split_threshold=.1, adj_type="expand")
+#' 
+#' # get the cell's neighbors
+#' pt = c(27000,10000)
+#' nbs = qt_get_neighbors(qt,pt)
+#' 
+#' # plot the neighbors
+#' qt_plot(qt,border_lwd=.3)
+#' points(pt[1],pt[2],col="black",bg="lightblue",pch=21)
+#' with(data.frame(nbs),rect(xmin,ymin,xmax,ymax,col="red",border="black",lwd=2))
+qt_get_neighbors = function(quadtree, pt){
+  if(!inherits(quadtree, "Rcpp_quadtree")) stop("'quadtree' must be a quadtree object (i.e. have class 'Rcpp_quadtree')")
+  return(quadtree$getNeighbors(pt))
+}
