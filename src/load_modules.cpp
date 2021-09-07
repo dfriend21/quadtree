ac41f84e6cbaf1feeac442134cbd1eb8102b9f80
Derek Friend <dfriend@Dereks-MacBook-Air.local>
2021-09-07 16:10:27 -0700

    removed old commented-out code

--- src/load_modules.cpp
+++ src/load_modules.cpp
@@ -2,12 +2,6 @@
 #include "QuadtreeWrapper.h"
 #include "ShortestPathFinderWrapper.h"
 #include "R_Interface.h"
-// #include "abm.hpp"
-// #include "abm2.hpp"
-
-//RCPP_EXPOSED_CLASS(NodeWrapper);
-
-//RCPP_EXPOSED_CLASS(QuadtreeWrapper);
 
 RCPP_MODULE(qt) {
   using namespace Rcpp;
@@ -26,21 +20,10 @@ RCPP_MODULE(qt) {
     .method("getNeighborIds", &NodeWrapper::getNeighborIds)
     .method("getNeighborVals", &NodeWrapper::getNeighborVals)
     .method("asVector", &NodeWrapper::asVector);
-  //.field("smallestChildSideLength", &Node::smallestChildSideLength)
-  //.field("hasChildren", &Node::hasChildren)
-  //.field("children", &Node::children);
   
   class_<QuadtreeWrapper>("CppQuadtree") 
     .constructor()
-    //.constructor<double, double, double, double, double>()
-      //.constructor<Rcpp::NumericMatrix, Rcpp::NumericVector, Rcpp::NumericVector, double, double, double>()
-                  // <Rcpp::NumericMatrix, Rcpp::NumericVector, Rcpp::NumericVector, Rcpp::Function, Rcpp::List, double, double);
-      // .constructor<Rcpp::NumericMatrix, Rcpp::NumericVector, Rcpp::NumericVector, Rcpp::Function, Rcpp::List, double, double>()
-         // .constructor<Rcpp::NumericMatrix, Rcpp::NumericVector, Rcpp::NumericVector, Rcpp::Function, Rcpp::List, Rcpp::Function, Rcpp::List, double, double>() //GAH WHY DO YOU ONLY ALLOW 6 PARAMETERS?!! @#*@^%#!!!! (https://rdrr.io/rforge/Rcpp/f/inst/doc/Rcpp-modules.pdf)
-    // .constructor<Rcpp::NumericVector, Rcpp::NumericVector, double, double, double, double>()
     .constructor<Rcpp::NumericVector, Rcpp::NumericVector, Rcpp::NumericVector, Rcpp::NumericVector, bool, bool>()
-    //.constructor<Rcpp::NumericVector, Rcpp::NumericVector, double>()
-      // .method("rangeLim", &QuadtreeWrapper::rangeLim)
     .method("asVector", &QuadtreeWrapper::asVector)
     .method("nNodes", &QuadtreeWrapper::nNodes)
     .method("root", &QuadtreeWrapper::root)
@@ -57,7 +40,6 @@ RCPP_MODULE(qt) {
     .method("getNbList", &QuadtreeWrapper::getNbList)
     .method("getShortestPathFinder", &QuadtreeWrapper::getShortestPathFinder)
     .method("copy", &QuadtreeWrapper::copy)
-    // .method("writeQuadtree", &QuadtreeWrapper::writeQuadtree)
     .method("setProjection", &QuadtreeWrapper::setProjection)
     .method("setOriginalValues", &QuadtreeWrapper::setOriginalValues)
     .method("extent", &QuadtreeWrapper::extent)
@@ -68,36 +50,14 @@ RCPP_MODULE(qt) {
     .method("maxCellDims", &QuadtreeWrapper::maxCellDims)
     .method("projection", &QuadtreeWrapper::projection);
   
-  // ShortestPathFinderWrapper(std::shared_ptr<Quadtree> quadtree, std::vector<double> startPoint);
-  // ShortestPathFinderWrapper(std::shared_ptr<Quadtree> quadtree, std::vector<double> startPoint, std::vector<double> xlims, std::vector<double> ylims);
-  // 
-  // Rcpp::NumericMatrix makeShortestPathNetwork();
-  // Rcpp::NumericMatrix getShortestPath(std::vector<double> endPoint);
   class_<ShortestPathFinderWrapper>("CppShortestPathFinder")
-    //.constructor<QuadtreeWrapper, Rcpp::NumericVector>()
-    //.constructor<QuadtreeWrapper, Rcpp::NumericVector, Rcpp::NumericVector, Rcpp::NumericVector>()
     .method("makeNetworkAll", &ShortestPathFinderWrapper::makeNetworkAll)
-    // .method("makeNetworkCost", &ShortestPathFinderWrapper::makeNetworkCost)
     .method("makeNetworkCostDist", &ShortestPathFinderWrapper::makeNetworkCostDist)
     .method("getShortestPath", &ShortestPathFinderWrapper::getShortestPath)
     .method("getAllPathsSummary", &ShortestPathFinderWrapper::getAllPathsSummary)
     .method("getStartPoint", &ShortestPathFinderWrapper::getStartPoint)
     .method("getSearchLimits", &ShortestPathFinderWrapper::getSearchLimits);
-    //.method("isValid", &ShortestPathFinderWrapper::isValid);
 
   function("readQuadtreeCpp", &QuadtreeWrapper::readQuadtree);
   function("writeQuadtreeCpp", &QuadtreeWrapper::writeQuadtree);
-  //.
-  //
-  //function("getNextPoint" , &getNextPoint);
-  //function("getAngle" , &getAngle);
-  //function("getPointsAroundPoint" , &getPointsAroundPoint);
-  //function("getAngleMoveProbs", &getAngleMoveProbs);
-  //function("getAngleOffsetVal", &getAngleOffsetVal);
-  //function("getRandomPointOnCircle", &getRandomPointOnCircle);
-  //function("getMaxDistPoint", &getMaxDistPoint);
-  //function("getRandomPointOnCircle", &getRandomPointOnCircle_cpp);
-  //function("moveTort", &moveTort);
-  //function("moveAgent_2", &moveAgent_2);
-  //function("getRandomPointsOnCircle", &getRandomPointsOnCircle);
 }
