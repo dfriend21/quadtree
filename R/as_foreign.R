f69e2d8737d5ccc0c9962727b464dd736912838b
Andrew Gene Brown <brown.andrewg@gmail.com>
2023-05-20 18:08:50 -0700

    Update foreign function conversions  - add `as(..., 'SpatRaster')`  - explicitly reference the quadtree:: methods (in case of masking by other packages)  - add `@examplesIf` to only run examples if terra and sf namespaces are available  - use `terra::vect(<data.frame>)` instead of updating values after using character method on WKT

--- R/as_foreign.R
+++ R/as_foreign.R
@@ -1,9 +1,11 @@
 # as foreign objects
 
+# x Quadtree object
 # returns a list with two elements:
 #   wkt -> character representing the WKT representation of the polygons
 #   vals -> numeric vector with the corresponding values
-as_wkt_list <- function(x){
+#   crs  -> WKT2019 string with CRS information of WKT polygons
+as_wkt_list <- function(x) {
   y <- as_data_frame(x)
   wkt <- sprintf("POLYGON ((%s %s, %s %s, %s %s, %s %s, %s %s))",
           y$xmin, y$ymin, 
@@ -12,22 +14,24 @@ as_wkt_list <- function(x){
           y$xmax, y$ymin,
           y$xmin, y$ymin)
   vals <- y$value
-  return(list(wkt = wkt, vals = vals))
+  return(list(wkt = wkt, vals = vals, crs = projection(x)))
 }
 
 #' Convert to other R spatial objects
 #'
 #' @param x Quadtree object
 #'
-#' @return an object of class \code{sf} or \code{SpatVector}, or a Well-Known Text (WKT)
-#'   \code{character} representation
+#' @return an object of class \code{sf} or \code{SpatVector}, or a Well-Known Text (WKT) \code{character} representation
+#' 
+#' @examplesIf all(sapply(c("sf"), function(x) !inherits(try(requireNamespace(x, quietly=TRUE), silent=TRUE), 'try-error')))
 #' @examples
 #' library(quadtree)
-#' data(habitat)
+#' habitat <- rast(system.file("extdata", "habitat.tif", package="quadtree"))
 #' 
 #' qt <- quadtree(habitat, .1)
 #' sf <- as(qt, "sf")
-#' v <- as(qt, "SpatVector")
+#' sr <- as(qt, "SpatRaster")
+#' sv <- as(qt, "SpatVector")
 #' ch <- as(qt, "character")
 #' @export
 #' @rdname as-foreign
@@ -36,7 +40,9 @@ as_sf <- function(x) {
     stop("package 'sf' is required to convert Quadtree to sf", call. = FALSE)
   }
   lst <- as_wkt_list(x)
-  v <- sf::st_as_sf(data.frame(value = lst$vals, geometry = sf::st_as_sfc(lst$wkt)), crs = projection(x))
+  v <- sf::st_as_sf(data.frame(value = lst$vals, 
+                               geometry = sf::st_as_sfc(lst$wkt)), 
+                    crs = lst$crs)
   return(v)
 }
 
@@ -47,8 +53,10 @@ as_vect <- function(x) {
     stop("package 'terra' is required to convert Quadtree to SpatVector", call. = FALSE)
   }
   lst <- as_wkt_list(x)
-  v <- terra::vect(lst$wkt, crs = projection(x))
-  values(v) <- data.frame(value = lst$vals)
+  v <- terra::vect(data.frame(value = lst$vals, 
+                              geometry = lst$wkt), 
+                   geom = "geometry",
+                   crs = lst$crs)
   return(v)
 }
 
@@ -56,10 +64,14 @@ as_vect <- function(x) {
 #' @rdname as-foreign
 as_character <- function(x) {
   lst <- as_wkt_list(x)
+  attr(lst$wkt, 'crs') <- lst$crs
   return(lst$wkt)
 }
 
 # create as(x, 'foo') coercion methods
-setAs("Quadtree", "sf", function(from) as_sf(from))
-setAs("Quadtree", "SpatVector",  function(from) as_vect(from))
-setAs("Quadtree", "character", function(from) as_character(from))
+setAs("Quadtree", "vector", function(from) quadtree::as_vector(from))
+setAs("Quadtree", "SpatRaster", function(from) quadtree::as_raster(from))
+setAs("Quadtree", "data.frame", function(from) quadtree::as_data_frame(from))
+setAs("Quadtree", "sf", function(from) quadtree::as_sf(from))
+setAs("Quadtree", "SpatVector",  function(from) quadtree::as_vect(from))
+setAs("Quadtree", "character", function(from) quadtree::as_character(from))
